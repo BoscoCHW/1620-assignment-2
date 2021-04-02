b490bba47ff455c01e02325c2a6cb5b9ac6f8156
Bosco_Acer <bosco.chw@gmail.com>
2021-04-02 00:30:35 -0700

    finish cancel note function

--- JS/app.js
+++ JS/app.js
@@ -1,18 +1,19 @@
 const changeThemebtn = document.querySelector('#change-theme-btn')
 function changeTheme() {
-  document.body.classList.toggle("dark-mode")
-  let mode = changeThemebtn.textContent
-  if (mode === "Light Mode") {changeThemebtn.textContent = "Dark Mode"}
-  else {changeThemebtn.textContent = "Light Mode"}
+    document.body.classList.toggle("dark-mode")
+    let mode = changeThemebtn.textContent
+    if (mode === "Light Mode") { changeThemebtn.textContent = "Dark Mode"} 
+    else { changeThemebtn.textContent = "Light Mode"} 
 }
-changeThemebtn.setAttribute('onclick','changeTheme()')
+changeThemebtn.onclick = changeTheme
 
 
 const addNotebtn = document.querySelector('#add-note-btn')
 function addNoteEditor() {
-    let newNote = document.createElement('div')
+    const newNote = document.createElement('div')
+    const addNotebtn = document.querySelector('#add-note-btn')
     newNote.className = 'new_note'
-    let innerhtml = `<p href="#">+ </p>
+    const innerhtml = `<p href="#">+ </p>
                     <textarea name="quicknote" rows="10" cols="33" placeholder="your note here"></textarea>
                     <div class="action">
                         <button href="#" id="save">save</button>
@@ -20,12 +21,11 @@ function addNoteEditor() {
                     </div>`
     newNote.innerHTML = innerhtml
     addNotebtn.parentElement.replaceChild(newNote, addNotebtn)
-    
+
+    document.querySelector('#cancel').addEventListener('click', cancelNewNote)
 }
-addNotebtn.setAttribute('onclick','addNoteEditor()')
+addNotebtn.addEventListener('click', addNoteEditor)
 
-
-const cancelbtn = document.querySelector('#cancel')
 function cancelNewNote() {
     let newNote = document.querySelector('div.new_note')
     let addNotebtn = document.createElement('button')
@@ -34,5 +34,5 @@ function cancelNewNote() {
     addNotebtn.innerText = '+ create a new note'
     newNote.parentElement.replaceChild(addNotebtn, newNote)
     
+    document.querySelector('#add-note-btn').onclick = addNoteEditor
 }
-cancelbtn.setAttribute('onclick','cancelNewNote()')
