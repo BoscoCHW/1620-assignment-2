78b13329c69a01ad0a7b3e18533150337e612f6c
Bosco_Acer <bosco.chw@gmail.com>
2021-04-03 09:13:47 -0700

    add function description

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,21 @@
-// function 1: change document theme
-// 1. detect trigger event: click on button
-// 2. change background color 
-// 3. change text color
+//function 1: switch between light and dark mode
+//1. select the button to change theme
+//2. when the button is clicked, toggle the dark-mode class for the body and apply dark mode style
+//3. change the button text to the other mode (dark -> light and light ->dark)
 
+//function 2: open note editor when "create new note" button is clicked
+//1. select "create new note" button
+//2. add event listener to the button, when clicked, display note taking area
+//  function: display note taking area
+//  2a. create a div to contain note taking area
+//  2b. add innerhtml for the components
+//  2c. replace the "add new note" button with the note taking area div
+//  2d. add evevnt listener for cancel and save button for their corresponding actions
 
-
-
-// function 2: 
+//function 3: close note taking area when the cancel button is clicked
+//1. create element for "create new note" button
+//2. add id to the button
+//3. add name to the button
+//4. select note taking area div
+//5. replace the note taking area div by the "create new note" button
+//6. add event listener for the "create new note" button
