6d13fa3aee35267f574258625231a7af1c23ec37
Bosco_Acer <bosco.chw@gmail.com>
2021-04-03 09:06:35 -0700

    added function description

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,7 @@
+//function 1: switch between light and dark mode
+//1. select the button to change theme
+//2. when the button is clicked, toggle the dark-mode class for the body and apply dark mode style
+//3. change the button text to the other mode (dark -> light and light ->dark)
 const changeThemebtn = document.querySelector('#change-theme-btn')
 changeThemebtn.onclick = function () {
     document.body.classList.toggle("dark-mode")
@@ -22,6 +26,15 @@ slideSideMenubtn.onclick = function () {
 
 }
 
+
+//function 2: open note editor when "create new note" button is clicked
+//1. select "create new note" button
+//2. add event listener to the button, when clicked, display note taking area
+//  function: display note taking area
+//  2a. create a div to contain note taking area
+//  2b. add innerhtml for the components
+//  2c. replace the "add new note" button with the note taking area div
+//  2d. add evevnt listener for cancel and save button for their corresponding actions
 const addNotebtn = document.querySelector('#add-note-btn')
 addNotebtn.addEventListener('click', addNoteEditor)
 
@@ -44,7 +57,13 @@ function addNoteEditor() {
     document.querySelector('#save').addEventListener('click', addNote)
 }
 
-
+//function 3: close note taking area when the cancel button is clicked
+//1. create element for "create new note" button
+//2. add id to the button
+//3. add name to the button
+//4. select note taking area div
+//5. replace the note taking area div by the "create new note" button
+//6. add event listener for the "create new note" button
 function closeNewNote() {
     
     const addNotebtn = document.createElement('button')
@@ -92,7 +111,10 @@ function createMenuItem(note) {
     let li = document.createElement('li')
     let btn = document.createElement('button')
     btn.textContent = note.title
+    btn.addEventListener('click', displayNote(note))
+
     li.appendChild(btn)
+
     return li
 }
 
